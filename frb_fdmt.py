3c442a329b190b915196cdaf186300e31a8a4caa
Kendrick Smith <kmsmith@astro.princeton.edu>
2015-09-12 13:35:13 -0400

    stay in sync with udpates to FDMT code

--- frb_fdmt.py
+++ frb_fdmt.py
@@ -7,13 +7,17 @@ def is_power_of_two(n):
 
 
 class fdmt:
-    def __init__(self, maxDT, recdepth):
+    def __init__(self, maxDT, recdepth, doWeighting=True):
         assert is_power_of_two(maxDT)  # not sure if this is necessary
         assert recdepth >= 0
 
         self.name = ('fdmt-%d-rec%d' % (maxDT,recdepth))
         self.maxDT = maxDT
         self.recdepth = recdepth
+        self.doWeighting = doWeighting
+
+        if not doWeighting:
+            self.name += '-noweight'
 
 
     def search_init(self, search_params):
@@ -25,7 +29,7 @@ class fdmt:
         self.search_result = 0.0
 
         self.debug_buffer_ndm = self.maxDT
-        self.debug_buffer_nt = search_params.nsamples_per_chunk - 2**self.recdepth + 1
+        self.debug_buffer_nt = search_params.nsamples_per_chunk // 2**self.recdepth
 
         search_max_delay = search_params.dt_sample * (self.maxDT-1) * 2**self.recdepth   # seconds
         search_max_dm = search_max_delay / 4.148806e3 / (search_params.band_freq_lo_MHz**(-2) - search_params.band_freq_hi_MHz**(-2))
@@ -41,6 +45,7 @@ class fdmt:
         cpuFDMT.fmax = self.search_params.band_freq_hi_MHz
         cpuFDMT.nchan = self.search_params.nchan
         cpuFDMT.maxDT = self.maxDT
+        cpuFDMT.doWeighting = self.doWeighting
         cpuFDMT.fs, cpuFDMT.df = np.linspace(cpuFDMT.fmin, cpuFDMT.fmax, cpuFDMT.nchan, endpoint=False, retstep=True)
         cpuFDMT.A = None
         cpuFDMT.B = None
@@ -52,10 +57,20 @@ class fdmt:
         assert self.search_params.nchunks == 1
 
         if debug_buffer is not None:
-            # FDMT ends up being run twice and we use too much memory, but that's OK on a debug path
-            t = self._assemble_debug_buffer(chunk, depth=self.recdepth)
-            assert t.shape == debug_buffer.shape
-            debug_buffer[:,:] = t[:,:]
+            # FDMT ends up being run twice, which is inefficient, but that's OK on a debug path
+            I = chunk
+            for i in xrange(self.recdepth):
+                # cut-and-paste from cpuFDMT.py
+                I = I[:,::2]+I[:,1::2] if (I.shape[1]%2 == 0) else I[:,:-1:2]+I[:,1::2]
+                if self.doWeighting: 
+                    I /= np.sqrt(2.)
+            
+            assert I.shape == (self.search_params.nchan, self.debug_buffer_nt)
+            debug_buffer[:,:] = cpuFDMT.fdmt(I, returnMaxSigma=False)[:,:]
+            
+            # mask entries which aren't searched by fdmt()
+            for i in xrange(debug_buffer.shape[0]):
+                debug_buffer[i,:i] = -1.0e30
 
         self.search_result = cpuFDMT.recursive_fdmt(chunk, depth=self.recdepth)
 
@@ -64,32 +79,3 @@ class fdmt:
         cpuFDMT.A = None
         cpuFDMT.B = None
         cpuFDMT.Q = None
-
-
-    def _assemble_debug_buffer(self, I, depth):
-        assert depth >= 0
-
-        if depth == 0:
-            ret = cpuFDMT.fdmt(I, returnMaxSigma=False)
-            for i in xrange(ret.shape[0]):
-                ret[i,:i] = -1.0e30
-                return ret
-
-        # cut-and-paste from cpuFDT.recursive_fdmt()
-        if I.shape[1]%2 == 0:
-            I2a = I[:,::2] + I[:,1::2]
-            I2b = I[:,1:-2:2] + I[:,2::2]
-        else:
-            I2a = I[:,:-1:2] + I[:,1::2]
-            I2b = I[:,1::2] + I[:,2::2]
-
-        ret_a = self._assemble_debug_buffer(I2a, depth-1)
-        ret_b = self._assemble_debug_buffer(I2b, depth-1)
-        assert ret_a.shape == (self.maxDT, I.shape[1]//2 - 2**(depth-1) + 1)
-        assert ret_b.shape == (self.maxDT, (I.shape[1]-1)//2 - 2**(depth-1) + 1)
-        
-        # interleave
-        ret = np.zeros((self.maxDT, I.shape[1] - 2**depth + 1), dtype=ret_a.dtype)
-        ret[:,::2] = ret_a[:,:]
-        ret[:,1::2] = ret_b[:,:]
-        return ret
